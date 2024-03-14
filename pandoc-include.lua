c58e7d10069190ef4055e326c06b6e237921cddf
777shuang <shuang777@outlook.jp>
2024-03-14 23:11:00 +0900

    拡張子の省略に対応

--- pandoc-include.lua
+++ pandoc-include.lua
@@ -1,8 +1,21 @@
+local function file_exists(name)
+    local f = io.open(name, 'r')
+    if f ~= nil then
+      io.close(f)
+      return true
+    else
+      return false
+    end
+  end
+
 function Para(el)
-    print(el)
     local str = el.content[1]
     if str.tag == "Str" and str.text:match("%[.*%]") then
-        return pandoc.CodeBlock(str.text:gsub('%[%[', ''):gsub('%]%]', ''), "include")
+        local path = str.text:gsub('%[%[', ''):gsub('%]%]', '')
+        if not file_exists(path) then
+            path = path .. ".md"
+        end
+        return pandoc.CodeBlock(path, "include")
     end
     return el
 end
